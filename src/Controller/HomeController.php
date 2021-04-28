3c3d5ab5cb8968ec5718f76aef4df5ff6d5428a9
mikhawa <michaeljpitz@gmail.com>
2021-04-28 11:55:13 +0200

    select section and section.html.twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,19 +15,44 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        // sections for menuhaut
+        // sections to menuhaut
         $sections = $this->getDoctrine()
             ->getRepository(Section::class)
             ->findAll();
 
-        // messages for content
+        // messages to content
         $messages = $this->getDoctrine()
             ->getRepository(Message::class)
             ->findBy([], ['idmessage' => 'desc']);
 
+        // Twig's view
         return $this->render('home/index.html.twig', [
             'sectionsMenuHaut' => $sections,
             'messages' => $messages,
+        ]);
+    }
+
+    /**
+     * @Route("/section/{slug}", name="section")
+     */
+    public function section(string $slug): Response
+    {
+        // sections to menuhaut
+        $sections = $this->getDoctrine()
+            ->getRepository(Section::class)
+            ->findAll();
+
+        // section to page
+        $section = $this->getDoctrine()
+            ->getRepository(Section::class)
+            ->findOneBy(["sectionslug" => $slug]);
+
+
+        // Twig's view
+        return $this->render('home/section.html.twig', [
+            'sectionsMenuHaut' => $sections,
+            'section' => $section,
+            //'messages' => $messages,
         ]);
     }
 }
