39fe7fd660083edcc65589777a5a5edc4f73afd9
mikhawa <michaeljpitz@gmail.com>
2021-05-07 12:18:13 +0200

    redirection after connexion

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -93,8 +93,8 @@ class UserAuthenticator extends AbstractFormLoginAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        // Redirection
+        return new RedirectResponse($this->urlGenerator->generate('home'));
     }
 
     protected function getLoginUrl()
